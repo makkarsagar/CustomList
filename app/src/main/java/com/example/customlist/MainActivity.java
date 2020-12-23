33bddc1a471a51efbbb1e6b3eb2b85008732053b
Sagar Makkar <makkarsagar96@gmail.com>
2020-12-23 14:08:00 -0500

    Custom List in Android Studio

--- app/src/main/java/com/example/customlist/MainActivity.java
+++ app/src/main/java/com/example/customlist/MainActivity.java
@@ -2,44 +2,134 @@ package com.example.customlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Contacts;
 import android.util.Log;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG="MainActivity";
+
+    private ListView lv;
+
+    String repname , owname;
+
+    private static String JSON_URL ="https://api.github.com/repositories";
+    ArrayList<HashMap<String,String>>List;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d(TAG, "onCreate : Started.");
 
-        ListView mListView = (ListView) findViewById(R.id.listView);
-        Person one = new Person ("grit","mojombo");
-        Person two = new Person ("merb-core","wycats");
-        Person three = new Person ("rubinius","rubinius");
-        Person four = new Person ("god","mojombo");
-        Person five = new Person ("jsawesome","vanpelt");
-        Person six = new Person ("jspec","wycats");
-        Person seven = new Person ("exception_logger","defunkt");
-        Person eight = new Person ("ambition","defunkt");
-        Person nine = new Person ("restful-authentication","technoweenic");
-        
-         ArrayList <Person> peopleList = new ArrayList<>()  ;
-         peopleList.add(one);
-        peopleList.add(two);
-        peopleList.add(three);
-        peopleList.add(four);
-        peopleList.add(five);
-        peopleList.add(six);
-        peopleList.add(seven);
-        peopleList.add(eight);
-        peopleList.add(nine);
+        List = new ArrayList<>();
+        lv = findViewById(R.id.listView);
 
-        PersonListAdapter adapter = new PersonListAdapter(this,R.layout.adapter_view_layout,peopleList);
-        mListView.setAdapter(adapter);
+        GetData getdata = new GetData();
+        getdata.execute();
+
+
     }
+
+    public class GetData extends AsyncTask<String, String, String>
+    {
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String current = "";
+
+            try {
+                URL url;
+                HttpURLConnection urlConnection = null;
+
+                try {
+                    url = new URL(JSON_URL);
+                    urlConnection = (HttpURLConnection) url.openConnection();
+
+                    InputStream in = urlConnection.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(in);
+
+                    int data = isr.read();
+                    while (data != -1) {
+                        current += (char) data;
+                        data = isr.read();
+                    }
+                    return current;
+
+
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                   if (urlConnection != null) {
+                      urlConnection.disconnect();
+                }
+            }
+        }
+        catch (Exception e){
+                e.printStackTrace();
+        }
+            return current;
+                }
+
+        @Override
+        protected void onPostExecute(String s){
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArray = jsonObject.getJSONArray("PeopleList");
+
+                for(int i = 0; i < jsonArray.length(); i++)
+                {
+                    JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                    repname = jsonObject1.getString("name");
+                    owname = jsonObject1.getString("full_name");
+
+                    HashMap<String,String> PeopleList = new HashMap<>();
+
+                    PeopleList.put("name", repname);
+                    PeopleList.put("full_name", owname);
+
+                    List.add(PeopleList);
+
+
+
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this,
+                    List,
+                    R.layout.adapter_view_layout,
+                    new String[] {"name", "full_name"},
+                    new int[]{R.id.tv1,R.id.tv2});
+
+                    lv.setAdapter(adapter);
+
+
+        }
+            }
+
+
+
 }
